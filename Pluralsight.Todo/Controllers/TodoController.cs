b911aa56304491df12f6255cb6052c02b934efaa
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-01 13:06:01 -0600

    Add project files.

---
+++ Pluralsight.Todo/Controllers/TodoController.cs
@@ -0,0 +1,105 @@
+﻿using Pluralsight.Todo.Models;
+using Pluralsight.Todo.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pluralsight.Todo.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: Todo
+        public ActionResult Index()
+        {
+            var repository = new TodoRepository();
+            var entities = repository.All();
+            var models = entities.Select(x => new TodoModel {
+                Id = x.RowKey,
+                Group = x.PartitionKey,
+                Content = x.Content,
+                Due = x.Due,
+                Completed = x.Completed,
+                Timestamp = x.Timestamp
+            });
+            return View(models);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(TodoModel model)
+        {
+            var repository = new TodoRepository();
+
+            repository.CreateOrUpdate(new TodoEntity {
+                PartitionKey = model.Group,
+                RowKey = Guid.NewGuid().ToString(),
+                Content = model.Content,
+                Due = model.Due
+            });
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ConfirmDelete(string id, string group)
+        {
+            var repository = new TodoRepository();
+            var item = repository.Get(group, id);
+
+            return View("Delete", new TodoModel
+            {
+                Id = item.RowKey,
+                Group = item.PartitionKey,
+                Content = item.Content,
+                Due = item.Due,
+                Completed = item.Completed,
+                Timestamp = item.Timestamp
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string id, string group)
+        {
+            var repository = new TodoRepository();
+            var item = repository.Get(group, id);
+            repository.Delete(item);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(string id, string group)
+        {
+            var repository = new TodoRepository();
+            var item = repository.Get(group, id);
+
+            return View(new TodoModel
+            {
+                Id = item.RowKey,
+                Group = item.PartitionKey,
+                Content = item.Content,
+                Due = item.Due,
+                Completed = item.Completed,
+                Timestamp = item.Timestamp
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TodoModel model)
+        {
+            var repository = new TodoRepository();
+            var item = repository.Get(model.Group, model.Id);
+            item.Completed = model.Completed;
+            item.Content = model.Content;
+            item.Due = model.Due;
+
+            repository.CreateOrUpdate(item);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Pluralsight.Todo/Repositories/TodoRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Azure.CosmosDB.Table;
+using Microsoft.Azure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pluralsight.Todo.Repositories
+{
+    public class TodoRepository
+    {
+        private CloudTable todoTable = null;
+        public TodoRepository()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=fekbergpluralsight;AccountKey=KEY_GOES_HERE;TableEndpoint=https://fekbergpluralsight.table.cosmosdb.azure.com:443");
+
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            todoTable = tableClient.GetTableReference("Todo");
+        }
+
+        public IEnumerable<TodoEntity> All()
+        {
+            var query = new TableQuery<TodoEntity>()
+                .Where(TableQuery.GenerateFilterConditionForBool(nameof(TodoEntity.Completed),
+                QueryComparisons.Equal,
+                false));
+
+            var entities = todoTable.ExecuteQuery(query);
+
+            return entities;
+        }
+
+        public void CreateOrUpdate(TodoEntity entity)
+        {
+            var operation = TableOperation.InsertOrReplace(entity);
+
+            todoTable.Execute(operation);
+        }
+
+        public void Delete(TodoEntity entity)
+        {
+            var operation = TableOperation.Delete(entity);
+
+            todoTable.Execute(operation);
+        }
+
+        public TodoEntity Get(string partitionKey, string rowKey)
+        {
+            var operation = TableOperation.Retrieve<TodoEntity>(partitionKey, rowKey);
+
+            var result = todoTable.Execute(operation);
+
+            return result.Result as TodoEntity;
+        }
+    }
+
+    public class TodoEntity : TableEntity
+    {
+        public string Content { get; set; }
+        public bool Completed { get; set; }
+        public string Due { get; set; }
+    }
+}
